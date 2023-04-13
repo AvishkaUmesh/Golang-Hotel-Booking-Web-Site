84d21719f614a67e70fe60f4acc61d3fc1a5228e
Avishka Umesh <umesh.avishka@gmail.com>
2023-04-13 18:42:12 +0530

    added form package tests

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -2,7 +2,6 @@ package forms
 
 import (
 	"fmt"
-	"net/http"
 	"net/url"
 	"strings"
 
@@ -24,8 +23,8 @@ func NewForm(data url.Values) *Form {
 }
 
 // Has checks that specific fields in a form submission are
-func (f *Form) Has(field string, r *http.Request) bool {
-	x := r.Form.Get(field)
+func (f *Form) Has(field string) bool {
+	x := f.Get(field)
 	if x == "" {
 		return false
 	}
@@ -43,8 +42,8 @@ func (f *Form) Required(fields ...string) {
 }
 
 // MinLength checks that a specific field in a form submission meets a minimum length requirement.
-func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	x := r.Form.Get(field)
+func (f *Form) MinLength(field string, length int) bool {
+	x := f.Get(field)
 	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
 		return false
