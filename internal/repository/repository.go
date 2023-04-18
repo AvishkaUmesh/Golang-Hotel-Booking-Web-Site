07e764ce6d4a7e84e93445c9a17a8b46d89eacb9
Avishka Umesh <umesh.avishka@gmail.com>
2023-04-18 19:44:41 +0530

    added insert reservation

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/AvishkaUmesh/Golang-Hotel-Booking-Web-Site/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+	InsertReservation(reservation models.Reservation) (int, error)
+	InsertRoomRestriction(restriction models.RoomRestriction) error
 }
